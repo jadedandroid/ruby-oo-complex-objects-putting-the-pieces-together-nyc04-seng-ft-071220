024f810815a5f2e812b568999ebdf07b5cdea8a2
jade decordova <lacroixjaded@gmail.com>
2020-08-04 23:14:42 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
-# Make your shoe class here!
+class Shoe
+  attr_reader :shoe, :material, :size, :color, :brand
+  def initialize(brand)
+    @brand = brand
+    @material = "leather"
+    @size = 7
+    @color = "Black"
+  end
+  
+  def cobble
+    puts""
+  
+  
+end
